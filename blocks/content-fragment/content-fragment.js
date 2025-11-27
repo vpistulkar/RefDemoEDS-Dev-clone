34c59ec41010306b6cc39d61201f3238c13bfe55
Kritika Goyal <kritikagoyal@adobe.com>
2025-11-27 15:26:07 +0530

    CTA Redirection Fix for CF block

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -139,6 +139,25 @@ export default async function decorate(block) {
           bannerDetailStyle = 'background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%) ,url('+imgUrl+');';
         }
 
+        // Derive CTA href: supports author-side paths/URLs and publish/EDS URLs
+        let ctaHref = '#';
+        const cta = cfReq?.ctaurl;
+        if (cta) {
+          if (typeof cta === 'string') {
+            // Absolute URL vs repository path
+            ctaHref = /^https?:\/\//i.test(cta) ? cta : `${isAuthor ? (aemauthorurl || '') : (aempublishurl || '')}${cta}`;
+          } else if (typeof cta === 'object') {
+            const authorUrl = cta._authorUrl;
+            const publishUrl = cta._publishUrl || cta._url;
+            const pathOnly = cta._path;
+            if (isAuthor) {
+              ctaHref = authorUrl || (pathOnly ? `${aemauthorurl || ''}${pathOnly}` : '#');
+            } else {
+              ctaHref = publishUrl || (pathOnly ? `${aempublishurl || ''}${pathOnly}` : '#');
+            }
+          }
+        }
+
       block.innerHTML = `<div class='banner-content block ${displayStyle}' data-aue-resource=${itemId} data-aue-label=${variationname ||"Elements"} data-aue-type="reference" data-aue-filter="contentfragment" style="${bannerContentStyle}">
           <div class='banner-detail ${alignment}' style="${bannerDetailStyle}" data-aue-prop="bannerimage" data-aue-label="Main Image" data-aue-type="media" >
                 <p data-aue-prop="title" data-aue-label="Title" data-aue-type="text" class='cftitle'>${cfReq?.title}</p>
@@ -146,7 +165,7 @@ export default async function decorate(block) {
                 
                 <div data-aue-prop="description" data-aue-label="Description" data-aue-type="richtext" class='cfdescription'><p>${cfReq?.description?.plaintext || ''}</p></div>
                  <p class="button-container ${ctaStyle}">
-                  <a href="${cfReq?.ctaUrl ? cfReq.ctaUrl : '#'}" data-aue-prop="ctaUrl" data-aue-label="Button Link/URL" data-aue-type="reference"  target="_blank" rel="noopener" data-aue-filter="page" class='button'>
+                  <a href="${ctaHref}" data-aue-prop="ctaurl" data-aue-label="Button Link/URL" data-aue-type="reference"  target="_blank" rel="noopener" data-aue-filter="page" class='button'>
                     <span data-aue-prop="ctalabel" data-aue-label="Button Label" data-aue-type="text">
                       ${cfReq?.ctalabel}
                     </span>
